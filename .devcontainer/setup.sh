fba3ec370b28b082eaf001595dd27eb83bb12342
Manoj Kumar Yadav <myadav27@outlook.com>
2025-03-04 04:15:36 +0000

    move setup command to bash file
    Signed-off-by: Ubuntu <newton-x1@svc-ado-007.ouans5uoojaeda1bo2vkwkz51c.bx.internal.cloudapp.net>
    Signed-off-by: Manoj Kumar Yadav <myadav27@outlook.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,3 +3,11 @@
 # Change device ownership
 sudo chown -R $REMOTE_USER:$REMOTE_GROUP $DEVICE
 
+# Check for the KVM device and adjust permissions.
+if [ -e /dev/kvm ]; then
+    sudo chown root:kvm /dev/kvm
+    sudo chmod 660 /dev/kvm
+    sudo usermod -aG kvm vscode
+else
+    echo 'KVM device not found, skipping...'
+fi
